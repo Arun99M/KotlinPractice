2f3f2be0e90552f67c599e4879704c4f16113261
arun <arunkumar.m5395@gmail.com>
2026-02-14 19:03:20 +0530

    TextView

--- app/src/main/java/com/example/kotlinandroidpractice/MainActivityimageview.kt
+++ app/src/main/java/com/example/kotlinandroidpractice/MainActivityimageview.kt
@@ -1,4 +1,31 @@
 package com.example.kotlinandroidpractice
 
-class MainActivityimageview {
-}
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivityimageview : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imageview)
+
+        val ivImage = findViewById<ImageView>(R.id.ivImage)
+        val btnToggle = findViewById<Button>(R.id.btnAddImage)
+
+        // Image click listener
+        btnToggle.setOnClickListener {
+
+            if (ivImage.visibility == android.view.View.VISIBLE) {
+                ivImage.visibility = android.view.View.GONE
+                btnToggle.text = "Enable Image"
+            } else {
+                ivImage.visibility = android.view.View.VISIBLE
+                btnToggle.text = "Disable Image"
+            }
+        }
+
+    }
+    }
